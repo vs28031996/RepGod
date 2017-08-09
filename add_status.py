5ebdafd594db6208ea8acd2320bccabb3dda01a2
Vinay sharma <vs28031996@gmail.com>
2017-08-09 16:07:54 +0530

    Add Status file

---
+++ add_status.py
@@ -0,0 +1,9 @@
+def add_status(status_message):
+    if status_message is None:
+        print " You don't have any status "
+    else:
+        print " Your current status is %s " % status_message
+
+
+
+
