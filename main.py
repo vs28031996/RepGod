a9433e8f47195864b985b44a140e5a3e0379245e
Vinay sharma <vs28031996@gmail.com>
2017-08-09 13:28:19 +0530

    gitignore file

--- main.py
+++ main.py
@@ -9,14 +9,35 @@ if len(spy_name) > 0:
     spy_is_online = False
     spy_age = raw_input("What is your age? ")
     spy_age = int(spy_age)
-    if 12 < spy_age < 50:
+    if type(spy_age) is int:
+        if 12 < spy_age < 50:
+            spy_rating = raw_input(" What is your spy rating: ")
+            spy_rating = float(spy_rating)
+            if spy_rating >4.5 :
+                print " Great ace!!"
+            elif 3.5 < spy_rating < 4.5:
+                print " You are Good one "
+            elif 2.5 < spy_rating < 3.5:
+                print " You can always do better"
+            else:
+                print " We can always use somebody to help in the office"
+            print " Alright " + spy_name + " i'd like to know more about you"
+            print " Welcome " + spy_name + " Glad to have u back"
 
-
-    print "Alright " + spy_name + " i'd like to know more about you"
-    print "Welcome " + spy_name + " Glad to have u back"
+        else:
+            print " You are not eligible to a spy"
+    else:
+        print " Please type a valid number "
 
 else:
     print "Invalid Name Try again"
+
+spy_is_online = True
+
+print " Authentication Complete . Welcome : %s Age : %d and rating of : %f " % (spy_name, spy_age, spy_rating)
+
+
+
 
 
 
