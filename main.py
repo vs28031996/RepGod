c91f58362271c12c2054daf5da43e5fdfd449845
Vinay sharma <vs28031996@gmail.com>
2017-08-09 15:47:39 +0530

    Main file

--- main.py
+++ main.py
@@ -1,49 +1,54 @@
-print "Let's get Started!"
-spy_name = raw_input("Provide your name here :")
+from spy_details import spy_name, spy_age, spy_salutation, spy_rating
+from start_chat import start_chat
 
-if len(spy_name) > 0:
-    spy_salutation = raw_input("What should we call you? : ")
-    spy_name = spy_salutation + " " + spy_name
-    spy_age = 0
-    spy_rating = 0.0
-    spy_is_online = False
-    spy_age = raw_input("What is your age? ")
-    spy_age = int(spy_age)
-    if type(spy_age) is int:
-        if 12 < spy_age < 50:
-            spy_rating = raw_input(" What is your spy rating: ")
-            spy_rating = float(spy_rating)
-            if spy_rating >4.5 :
-                print " Great ace!!"
-            elif 3.5 < spy_rating < 4.5:
-                print " You are Good one "
-            elif 2.5 < spy_rating < 3.5:
-                print " You can always do better"
+question = "Continue as %s %s (Y/N)" % (spy_salutation, spy_name)
+existing = raw_input(question)
+
+# validating user's input
+if existing == "Y" or existing == "y":
+    start_chat(spy_name, spy_age, spy_rating)
+
+elif existing == "N" or existing == "n":
+    spy_name = raw_input("Provide your name here :")
+
+    if len(spy_name) > 0:
+        spy_salutation = raw_input("What should we call you? : ")
+        spy_name = spy_salutation + " " + spy_name
+        spy_age = 0
+        spy_rating = 0.0
+        spy_is_online = False
+        spy_age = raw_input("What is your age? ")
+        spy_age = int(spy_age)
+        if type(spy_age) is int:
+            if 12 < spy_age < 50:
+                spy_rating = raw_input(" What is your spy rating: ")
+                spy_rating = float(spy_rating)
+                if spy_rating > 4.5:
+                    print " Great ace!!"
+                elif 3.5 < spy_rating < 4.5:
+                    print " You are Good one "
+                elif 2.5 < spy_rating < 3.5:
+                    print " You can always do better"
+                else:
+                    print " We can always use somebody to help in the office"
+                print " Alright " + spy_name + " i'd like to know more about you"
+                print " Welcome " + spy_name + " Glad to have u back"
+
             else:
-                print " We can always use somebody to help in the office"
-            print " Alright " + spy_name + " i'd like to know more about you"
-            print " Welcome " + spy_name + " Glad to have u back"
-
+                print " You are not eligible to a spy"
         else:
-            print " You are not eligible to a spy"
+            print " Please type a valid number "
+
     else:
-        print " Please type a valid number "
+        print "Invalid Name Try again"
+
+
 
 else:
-    print "Invalid Name Try again"
+    print " Wrong choice.Try again"
+
+print "Let's get Started!"
 
 spy_is_online = True
 
 print " Authentication Complete . Welcome : %s Age : %d and rating of : %f " % (spy_name, spy_age, spy_rating)
-
-
-
-
-
-
-
-
-
-
-
-
