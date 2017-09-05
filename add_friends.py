39fbdd63ab62b87fb7120729ccb84230f2ea3035
Vinay sharma <vs28031996@gmail.com>
2017-09-05 06:20:27 +0530

    minor upadte

--- add_friends.py
+++ add_friends.py
@@ -1,5 +1,28 @@
+from spy_details import spy
+from globals import friends
+
 def add_friend():
-    friend_name = []
-    friend_age = []
-    friend_is_online = []
-    friend_rating = []
+
+    new_friend = {
+        'name': '',
+        'salutation': '',
+        'age': 0,
+        'is_online': False,
+        'rating': 0,
+        }
+    new_friend['name'] = raw_input("Add your friend . Name : ")
+    new_friend['salutation'] = raw_input("Are they Mr. or Mrs. : ")
+    new_friend['name'] = new_friend['salutation'] + " " + new_friend['name']
+
+    new_friend['age'] = int(raw_input("Age: "))
+    new_friend['rating'] = float(raw_input("Spy Rating: "))
+
+    if len(new_friend['name'])>0 and  new_friend['age']>12 and new_friend['rating']>=spy['rating']:
+        friends.append(new_friend)
+    else:
+        print "Sorry!! invalid entry "
+
+    return len(friends)
+
+
+    
