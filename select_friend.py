830b6934491b3e95e9462c9b48263d00463e2243
Vinay sharma <vs28031996@gmail.com>
2017-09-05 07:10:04 +0530

    minor updates

--- select_friend.py
+++ select_friend.py
@@ -2,6 +2,15 @@ from globals import friends
 
 
 def select_friend():
+    item_number = 0
+    for friend in friends:
+        print " %d. %s aged %d with rating %.2f is online " % (item_number+1, friend['name'], friend['age'], friend['rating'])
+        item_number = item_number + 1
+        friend_choice = int(raw_input("Choose from your friends: "))
+        friend_choice_position = friend_choice - 1
+        return friend_choice_position
+    
+
 
 
 
