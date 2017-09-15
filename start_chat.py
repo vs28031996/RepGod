10bf1ff675131f798b3af8c356d27d3afcab1225
Vinay sharma <vs28031996@gmail.com>
2017-09-16 00:14:58 +0530

    start chat

--- start_chat.py
+++ start_chat.py
@@ -3,6 +3,7 @@ from add_status import add_status, status
 from add_friends import add_friend
 from spy_details import  spy
 from send_message import send_message
+from read_message import read_message
 
 
 def start_chat(name, age, rating):
@@ -15,7 +16,7 @@ def start_chat(name, age, rating):
     show_menu = True
     while show_menu:
         menu_choices = " What do you want to do? : \n 1. Add Status  \n 2. Add a friend \n 3. Send a secret message " \
-                       "\n 4. Read a secret message \n 5. Read Chats from user \n 2. Close Application \n "
+                       "\n 4. Read a secret message \n 5. Read Chats from user \n 6. Close Application \n "
         result = int(raw_input(menu_choices))
         # validating user input
         if result == 1:
@@ -33,6 +34,12 @@ def start_chat(name, age, rating):
 
         elif result == 3:
             send_message()
+
+        elif result == 4:
+            read_message()
+
+        elif result == 5:
+            
 
         else:
             print " Wrong choice .Try again"
